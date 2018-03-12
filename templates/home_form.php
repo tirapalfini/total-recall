36ef8e892f94e46413c2853dbb0a0bd0a86719e5
Tira Palfini <wicklant@oregonstate.edu>
2018-03-12 15:56:13 -0700

    shopping list table added to home.php. no js or css performed. table for products created in database - followed HW4 UML for design

--- templates/home_form.php
+++ templates/home_form.php
@@ -1,13 +1,35 @@
 <div id="main-content" class="container">
-
-			<!-- Create default search form -->
-            <div class="row">
-				<h1 class="centered-header landing-text">Total Recall</h1>
-                <hr class="underline">
-            </div>
-
-			<!-- Dummy button for triggering modal -->
-			<div class="row landing-button">
-					<a href="logout.php"><button type="button" class="btn btn-secondary btn-lg btn-block btn-blue" id="logout">Logout</button></a>
-			</div>
+	<!-- Title -->
+    <div class="row">
+		<h1 class="centered-header landing-text">Total Recall</h1>
+        <hr class="underline">
+    </div>
+    <!-- Shopping List -->
+    <div>
+        <table class="table" id="shopping-list">
+        	<thead>
+        		<tr>
+        			<th>Name</th>
+        			<th>Quantity</th>
+        			<th>Description</th>
+        		</tr>
+        	</thead>
+        	<tbody align="center">
+        		<?php
+        			foreach($positions as $position)
+        			{
+        				print ("<tr>");
+    					print ("<td>{$position["name"]}</td>");
+    					print ("<td>{$position["quanitity"]}</td>");
+   						print ("<td>{$position["description"]}</td>");
+    					print ("</tr>");
+        			}
+        		?>
+        	</tbody>
+        </table>
+    </div>
+	<!-- Dummy button for triggering modal -->
+	<div class="row landing-button">
+			<a href="logout.php"><button type="button" class="btn btn-secondary btn-lg btn-block btn-blue" id="logout">Logout</button></a>
+	</div>
 </div>
